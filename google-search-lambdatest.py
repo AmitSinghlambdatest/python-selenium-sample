54ad5ad1f5223da938bbeac123893c8c49834ffb
AmitSinghlambdatest <72015691+AmitSinghlambdatest@users.noreply.github.com>
2020-12-16 17:24:21 +0530

    Update google-search-lambdatest.py

--- google-search-lambdatest.py
+++ google-search-lambdatest.py
@@ -63,7 +63,7 @@ class LTAutomate(unittest.TestCase):
         print(url)
         self.driver = webdriver.Remote(
             desired_capabilities=desired_cap,
-	    #desired_capabilities["fixedIP"]="IP Address"
+	    desired_capabilities["fixedIP"]="23.105.164.144"
             command_executor= url
         )
 
